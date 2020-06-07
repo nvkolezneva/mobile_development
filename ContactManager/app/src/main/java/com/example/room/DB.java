a0e0be06efe4d63ffbc62064aec087e8a02666d4
nvkolezneva <nvkolezneva@gmail.com>
2020-06-07 14:09:36 +0300

    Добаблена лабораторная №4

---
+++ ContactManager/app/src/main/java/com/example/room/DB.java
@@ -0,0 +1,9 @@
+package com.example.room;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class DB extends RoomDatabase {
+    public abstract UserDao userDao();
+}
---
+++ ContactManager/app/src/main/java/com/example/room/User.java
@@ -0,0 +1,17 @@
+package com.example.room;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+    @PrimaryKey
+    public int id;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "email")
+    public String email;
+}
